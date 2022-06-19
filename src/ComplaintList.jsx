db9343540052173081b96fc08f7be5c4c850aead
baolong3251 <baolong3251@gmail.com>
2022-06-19 17:02:28 +0700

    third

--- src/ComplaintList.jsx
+++ src/ComplaintList.jsx
@@ -1,72 +1,142 @@
-import React, { useReducer, useState } from 'react'
-import { Space, Table, Tag, Button, message, Col, Row, Typography, Pagination, Switch } from 'antd';
-import { DeleteTwoTone, LeftOutlined, RightOutlined, FormOutlined } from '@ant-design/icons';
-import EditThing from './EditThing';
+import React, { useState } from 'react'
+import { Space, Table, Button, Col, Row, Typography, Pagination } from 'antd';
+import { LeftOutlined, RightOutlined, DownOutlined } from '@ant-design/icons';
 import SearchForm from './SearchForm';
 import { Link } from "react-router-dom"
 import "./style_tableComplaintList.css"
 import Data from "./response_1655451432472.json"
+import moment from 'moment'
 
 const ComplaintList = () => {
-    const [show, setShow] = useState(true)
-    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
-
-    const onChange = (checked) => {
-        console.log(`switch to ${checked}`);
-    };
-
-    console.log(Data.data)
-
 
     const columns = [
         {
-            title: 'Complaint ID',
+            title:
+                ({ sortColumns }) => {
+                    const sortedColumn = sortColumns?.find(({ column }) => column.key === "complaint_id");
+                    return (
+                        <div className='complaintList-title'>
+                            Complaint ID
+
+                            {sortedColumn ? (
+                                sortedColumn.order === "descend" ? (
+                                    <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#4d98d1", fontSize: "12px" }} />
+                                ) : (
+                                    <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#bfbfbf", fontSize: "12px" }} />
+                                )
+                            ) : <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#bfbfbf", fontSize: "12px" }} />}
+                        </div>
+                    )
+                },
             dataIndex: 'complaint_id',
             defaultSortOrder: 'descend',
             sorter: (a, b) => a.complaint_id - b.complaint_id,
             sortDirections: ['descend'],
             key: 'complaint_id',
-            render: (text) => <a>{text}</a>,
+            render: (text) => <a className='complaintList-text' style={{color: "#4b89ad", textDecoration: "underline", fontWeight: "500"}}>{text}</a>,
         },
         {
-            title: 'JPOS/CIPI',
+            title: 
+                ({ sortColumns }) => {
+                    const sortedColumn = sortColumns?.find(({ column }) => column.key === "jpos_cipi_id");
+                    return (
+                        <div className='complaintList-title'>
+                            JPOS/CIPI
+
+                            {sortedColumn ? (
+                                sortedColumn.order === "ascend" ? (
+                                    <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#4d98d1", fontSize: "12px" }} />
+                                ) : (
+                                    <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#bfbfbf", fontSize: "12px" }} />
+                                )
+                            ) : <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#bfbfbf", fontSize: "12px" }} />}
+                        </div>
+                    )
+                },
             dataIndex: 'jpos_cipi_id',
             key: 'jpos_cipi_id',
+            
+            sorter: (a, b) => a.jpos_cipi_id.length - b.jpos_cipi_id.length,
+            sortDirections: ['ascend'],
+            render: (text) => <div className='complaintList-text'>{text}</div>
         },
         {
-            title: 'Status',
+            title:
+                ({ sortColumns }) => {
+                    const sortedColumn = sortColumns?.find(({ column }) => column.key === "complaint_status");
+                    return (
+                        <div className='complaintList-title'>
+                            Status
+
+                            {sortedColumn ? (
+                                sortedColumn.order === "ascend" ? (
+                                    <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#4d98d1", fontSize: "12px" }} />
+                                ) : (
+                                    <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#bfbfbf", fontSize: "12px" }} />
+                                )
+                            ) : <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#bfbfbf", fontSize: "12px" }} />}
+                        </div>
+                    )
+                },
             dataIndex: 'complaint_status',
             key: 'complaint_status',
+            
+            sorter: (a, b) => a.complaint_status.length - b.complaint_status.length,
+            sortDirections: ['ascend'],
             render: (_, record) => (
-                <><div style={{ textAlign: "left!important" }}>
-                    <Space style={{ display: "flex", justifyContent: "flex-start", alignItems: "center", textAlign: "left" }}><div className='dot_status'
-                        style={{
-                            color:
-                                `${record.complaint_status === "New" ? "green" : record.complaint_status.length > 6 ? '#ef00ff' : '#088aa1'}`,
-                            fontSize: '20px',
-                            fontWeight: "bold",
-                            transform: "translate(0, -2.5px)"
-                        }}
-                    > •
-                    </div>
-                        {record.complaint_status}
+                <>
+                    <Space className='complaintList-status' style={{ display: "flex", justifyContent: "flex-start", alignItems: "center", textAlign: "left" }}>
+                        <div
+                            style={{
+                                color:
+                                    `${record.complaint_status === "New" ? "green" : record.complaint_status.length > 6 ? '#ef00ff' : '#088aa1'}`,
+                                fontSize: '20px',
+                                fontWeight: "bold",
+                                transform: "translate(0, -2.5px)"
+                            }}
+                        > 
+                        •
+                        </div>
+                        <div className='complaintList-statusText'>
+                            {record.complaint_status}
+                        </div>
                     </Space>
-                </div>
                 </>
             ),
         },
         {
-            title: 'Division',
+            title:
+                ({ sortColumns }) => {
+                    const sortedColumn = sortColumns?.find(({ column }) => column.key === "product");
+                    return (
+                        <div className='complaintList-title'>
+                            Division
+
+                            {sortedColumn ? (
+                                sortedColumn.order === "descend" ? (
+                                    <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#4d98d1", fontSize: "12px" }} />
+                                ) : (
+                                    <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#bfbfbf", fontSize: "12px" }} />
+                                )
+                            ) : <DownOutlined className='sortIcon' style={{ marginLeft: "4px", color: "#bfbfbf", fontSize: "12px" }} />}
+                        </div>
+                    )
+                },
             key: 'product',
             dataIndex: 'product',
+            
+            sorter: (a, b) => a.product.division.slice(9).length - b.product.division.slice(9).length,
+            sortDirections: ['descend'],
             render: (_, record) => {
                 return (
-                    record.product.division.slice(9)
+                    <div className='complaintList-text'>
+                        {record.product.division.slice(9)}
+                    </div>
                 )
             },
         },
         {
-            title: 'Complaint Assigned To',
+            title: (<div className='complaintList-title'>Complaint Assigned To</div>),
             key: 'complaint_assign_to',
             dataIndex: 'complaint_assign_to',
         },
@@ -76,128 +146,70 @@ const ComplaintList = () => {
             dataIndex: 'created_date',
             render: (_, record) => {
                 return (
-                    record.created_date //format...
+                    <div className='complaintList-text'>
+                        {moment(record.created_date).format('YYYY/MM/DD')}
+                    </div>
                 )
             }
         },
         {
-            title: 'Country',
+            title: (<div className='complaintList-title'>Country</div>),
             key: 'country_of_event',
             dataIndex: 'country_of_event',
+            render: (text) => <div className='complaintList-text'>{text}</div>,
         },
         {
-            title: 'Product Name',
+            title: (<div className='complaintList-title'>Product Name</div>),
             key: 'product_name',
             dataIndex: 'product',
             render: (_, record) => {
                 return (
-                    record.product.product_name
+                    <div className='complaintList-text'>
+                        {record.product.product_name}
+                    </div>
                 )
             },
         },
         {
-            title: 'OEM Supplier',
+            title: (<div className='complaintList-title'>OEM Supplier</div>),
             key: 'oem',
             dataIndex: 'product',
             render: (_, record) => {
                 return (
-                    record.product.oem
+                    <div className='complaintList-text'>
+                        {record.product.oem}
+                    </div>
                 )
             },
         },
         {
-            title: 'MDV',
+            title: (<div className='complaintList-title'>MDV</div>),
             key: 'mdv',
             dataIndex: 'mdv',
+            render: (text) => <div className='complaintList-text'>{text}</div>,
         },
         {
-            title: 'Product Return',
+            title: (<div className='complaintList-title'>Product Return</div>),
             key: 'productReturn',
             dataIndex: 'productReturn',
+            render: (text) => <div className='complaintList-text'>{text}</div>,
         },
         {
-            title: 'CRL',
+            title: (<div className='complaintList-title'>CRL</div>),
             key: 'crl',
             dataIndex: 'crl',
+            render: (text) => <div className='complaintList-text'>{text}</div>,
         },
         {
-            title: 'GFE',
+            title: (<div className='complaintList-title'>GFE</div>),
             key: 'gfe',
             dataIndex: 'gfe',
+            render: (text) => <div className='complaintList-text'>{text}</div>,
         },
     ];
     const [data, setData] = useState(
         Data.data
-        //     [
-        //     {
-        //         key: '1',
-        //         complaint_id: 'OCS-0000069',
-        //         jposCipi: "696969",
-        //         complaint_status: 'Processing',
-        //         product: {division: "PI"},
-        //         cAssignedTo: "mmm",
-        //         createdDate: "2022-05-19",
-        //         country: "Japan",
-        //         productName: "Jupiter Max",
-        //         oemSupplier: "bstar demo",
-        //         mdv: "",
-        //         productReturn: "Shipped",
-        //         crl: "In-Progress",
-        //         gfe: "Open",
-        //     },
-        //     {
-        //         key: '2',
-        //         complaint_id: 'OCS-0000069',
-        //         jposCipi: "696969",
-        //         complaint_status: 'New',
-        //         product: {division: "cc"},
-        //         cAssignedTo: "mmm",
-        //         createdDate: "2022-05-19",
-        //         country: "Japan",
-        //         productName: "Jupiter Max",
-        //         oemSupplier: "bstar demo",
-        //         mdv: "",
-        //         productReturn: "Shipped",
-        //         crl: "In-Progress",
-        //         gfe: "Open",
-        //     },
-        //     {
-        //         key: '3',
-        //         complaint_id: 'OCS-0000069',
-        //         jposCipi: "696969",
-        //         complaint_status: 'Reopen',
-        //         product: {division: "PI"},
-        //         cAssignedTo: "mmm",
-        //         createdDate: "2022-05-19",
-        //         country: "Japan",
-        //         productName: "Jupiter Max",
-        //         oemSupplier: "bstar demo",
-        //         mdv: "",
-        //         productReturn: "Shipped",
-        //         crl: "In-Progress",
-        //         gfe: "Open",
-        //     },
-        // ]
     );
-
-    const handleDelete = (id) => {
-        var newArray = data
-        newArray = newArray.filter(x => x.key != id)
-        setData(newArray)
-        success()
-    }
-
-    const changeData = (id, value) => {
-        var newArray = data
-        var objIndex = newArray.findIndex((obj => obj.key == id));
-        newArray[objIndex].name = value
-        setData(newArray)
-        forceUpdate()
-    }
-
-    const success = () => {
-        message.success('Success');
-    };
 
     return (
         <>
@@ -234,7 +246,6 @@ const ComplaintList = () => {
                 </Col>
             </Row>
             <div style={{ padding: "10px" }}>
-                {/* <Button type="primary" onClick={() => setShow(!show)}>Hide</Button> */}
                 <Table
                     pagination={{ style: { display: 'none' } }}
                     columns={columns}
